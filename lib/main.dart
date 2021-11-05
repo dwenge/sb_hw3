41515486aebf331d76aabed8db9c0fd04f56422c
Dwenge <shirinkin.denis@bk.ru>
2021-11-05 22:22:42 +0500

    task 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Skillbox Homework 3',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: const HomePage(),
     );
@@ -29,11 +30,37 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text('Skillbox Homework 3'),
+          children: [
+            tile(
+              SvgPicture.asset('assets/svg/manjaro-logo.svg'),
+            ),
+            tile(
+              SvgPicture.network(
+                  'https://raw.githubusercontent.com/dnfield/flutter_svg/7d374d7107561cbd906d7c0ca26fef02cc01e7c8/example/assets/flutter_logo.svg'),
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  Container tile(Widget child) {
+    return Container(
+      width: 150,
+      height: 150,
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(15),
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromARGB(10, 0, 0, 0),
+            blurRadius: 5,
+          )
+        ],
+      ),
+      child: child,
     );
   }
 }
